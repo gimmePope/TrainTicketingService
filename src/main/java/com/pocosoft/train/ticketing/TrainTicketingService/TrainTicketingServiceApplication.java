3db1214725445cb20ec6a3e8c6662f37af524692
Otulana Olujimi <jimiotulana@gmail.com>
2023-01-05 07:53:17 +0100

    Project startup, initial commit

---
+++ src/main/java/com/pocosoft/train/ticketing/TrainTicketingService/TrainTicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pocosoft.train.ticketing.TrainTicketingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pocosoft/train/ticketing/TrainTicketingService/TrainTicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocosoft.train.ticketing.TrainTicketingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
